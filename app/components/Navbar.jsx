b6a255b24882140b101dd2ed24662ce009d72d20
aqib-9 <rylegendaqib@gmail.com>
2025-07-07 14:32:41 +0530

    Initial commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import { auth, signOut, signIn } from "@/auth";
+import Link from "next/link";
+
+const Navbar = async () => {
+  const session = await auth();
+
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <img src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+
+        <div className="flex items-center gap-5 text-black">
+          {session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+
+              {/* Server action for signOut */}
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">
+                  <span>LogOut</span>
+                </button>
+              </form>
+
+              <Link href={`/user/${session.user.id}`}>
+                <span>{session.user.name}</span>
+              </Link>
+            </>
+          ) : (
+            // Server action for signIn
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit">
+                <span>Login</span>
+              </button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
